801393e59fba181690abce7fda27710d7a6fb7f3
Mitch Joa <mitch.joa@yello.co>
2017-08-08 21:17:07 -0500

    Change a before(:all) create to become a before(:each)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,12 +12,22 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   I18n.enforce_available_locales = false
   config.treat_symbols_as_metadata_keys_with_true_values = true
   #config.profile_examples = true # uncomment if profiling is necessary
 
-  config.before(:all) { Footprints::Repository.craftsman.destroy_all; Footprints::Repository.applicant.destroy_all; Footprints::Repository.assigned_craftsman_record.destroy_all }
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :deletion
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 OmniAuth.config.test_mode = true
