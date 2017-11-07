a2f899aa4ab6b00139ea599f3e5768baa9cb9113
Trent Kan <trent.kan@yello.co>
2017-11-07 13:31:43 -0600

    Add field partial and add answer partial

--- app/controllers/admin/forms_controller.rb
+++ app/controllers/admin/forms_controller.rb
@@ -6,5 +6,23 @@ module Admin
       @fields    = ::FormService.fields_for_form_type(@form_type)
     end
 
+    def create
+      Field.delete_all
+      FieldChoice.delete_all
+
+      params[:questions].each do |question_number, question_data|
+        next if question_data[:name].blank?
+        answers = (question_data[:answers] || []).map { |k, a| a[:name] if a[:name].present? }
+        field = Field.create!(name: question_data[:name], form_type: params[:form_type], has_choices: answers.present?)
+        if field.has_choices
+          answers.each do |answer|
+            FieldChoice.create!(name: answer, field_id: field.id)
+          end
+        end
+      end
+
+      flash[:notice] = 'Form saved!'
+      redirect_to admin_show_form_path(form_type: params[:form_type])
+    end
   end
 end
