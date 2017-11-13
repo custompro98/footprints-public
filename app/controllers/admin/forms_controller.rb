d93d44216224534814a607024f0ff88cd0caa60a
Mitch Joa <mitch.joa@yello.co>
2017-11-12 20:38:18 -0600

    Dont refresh the page on submitting application form, stick flash container to top of window

--- app/controllers/admin/forms_controller.rb
+++ app/controllers/admin/forms_controller.rb
@@ -2,14 +2,18 @@ module Admin
   class FormsController < ApplicationController
 
     def show
-      @form_type = params[:form_type]
-      @fields    = ::FormService.fields_for_form_type(@form_type)
+      respond_to do |format|
+        format.js { flash.now[:notice] = 'Form saved!' }
+        format.html do
+          @form_type = params[:form_type]
+          @fields    = ::FormService.fields_for_form_type(@form_type)
+        end
+      end
     end
 
     def create
       ::CreateFormService.create_form(params[:form_type], params[:questions])
 
-      flash[:notice] = 'Form saved!'
       redirect_to admin_show_form_path(form_type: params[:form_type])
     end
   end
